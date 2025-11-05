7f69a3f17f1d789feb25d8c94e4df56d4552964f
Divya Asthana <divyakumar12a48@gmail.com>
2025-11-06 01:47:55 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,21 +6,19 @@ public class InsertionSort {
         } 
         System.out.println();           
         }
-        public void sort (int[]arr){
+        public void sort (int[]arr){         
             int n = arr.length;
             for (int i = 0; i < n ; i++) {
                 int temp = arr[i];
                 int j = i-1;
-                while(j>=0  && arr[j] > temp){// arr[j] < temp will provide decreasing order
+                while(j>=0  && arr[j] > temp){ // arr[j] < temp will provide decreasing order
                     arr[j+1] = arr[j];
-                    j = j-1;
+                    j = j-1 ;
                 }
                  arr[j+1] = temp;
-                 
-                
             }
-        }
-         public static void main(String[] args) {
+        }             
+            public static void main(String[] args) {
             int []arr = {1,5,9,2,10};
             InsertionSort obj = new InsertionSort();
             obj.sort(arr);
